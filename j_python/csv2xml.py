74b2d0ce1bba5b9245d978338903cae981078553
JW <jaewoolee@tecace.com>
2017-05-16 18:09:45 -0700

    Add python stuff!

---
+++ j_python/csv2xml.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import csv
+
+if len(sys.argv) != 2:
+    os._exit(1)
+
+path = sys.argv[1]  # get folder as a command line argument
+
+os.chdir(path)
+
+csvFiles = []
+for f in os.listdir('.'):
+    if f.endswith('.csv') or f.endswith('.CSV'):
+        csvFiles.append(f)
+
+# csvFiles = [f for f in os.listdir('.') if f.endswith('.csv') or f.endswith('.CSV')]
+
+for csvFile in csvFiles:
+    xmlFile = csvFile[:-4] + '.xml'
+    csvData = csv.reader(open(csvFile))
+    xmlData = open(xmlFile, 'w')
+    # xmlData.write('<?xml version="1.0"?>' + "\n") # Commented out
+    # there must be only one top-level tag
+    xmlData.write('<resources>' + "\n")
+    rowNum = 0
+
+    # First row of the csv files must be the header
+    for row in csvData:
+        if rowNum == 0:
+            tags = row
+            # replace spaces w/ underscores in tag names
+            for i in range(len(tags)):
+                tags[i] = tags[i].replace(' ', '_')
+        else:
+            print(row)
+            xmlData.write('    <string name=',)
+            # for i in range(len(tags)):
+            for i in range(1):
+                xmlData.write("\"" + tags[i] + '_' + row[i] + "\">" + row[1] + '</string>' + "\n")
+        rowNum += 1
+        
+    xmlData.write('</resources>' + "\n")
+    xmlData.close()
