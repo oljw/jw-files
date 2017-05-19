b3ff7808742aa79fc48d12a8a16e35af006a3cc9
JW <jaewoolee@tecace.com>
2017-05-18 18:12:56 -0700

    Python xls2xml almost done.

--- j_python/xls2xml.py
+++ j_python/xls2xml.py
@@ -1,18 +1,24 @@
 import sys
 import os
 import xlrd
+import tkinter as Tk
+import tkinter.filedialog as fd
 
-if len(sys.argv) != 2:
-    os._exit(1)
-path = sys.argv[1]  # get folder as a command line argument
+path = fd.askdirectory()
+
 os.chdir(path)
 
 xlsFiles = [files for files in os.listdir('.') if files.endswith('.xls') or files.endswith('.XLS')]
 
+keys = []
+values = []
+
 def main():
+    print("main called")
     for xlsFile in xlsFiles:
         workbook = xlrd.open_workbook(xlsFile)
         sheets = workbook.sheet_names()
+        prevKey = ""
 
         for langs in range(len(sheets)):
             sheet = workbook.sheet_by_index(langs)
@@ -23,50 +29,26 @@ def main():
             xmlData.write('<resources>' + "\n")    
 
             for row_num in range(sheet.nrows):
+                if row_num == 0:
+                    continue
                 key = sheet.row_values(row_num)[0]
-                value = sheet.row_values(row_num)[1]
+                value = sheet.row_values(row_num)[1].replace(r"'", r"\'")
 
                 xmlData.write('    <string name=',)
                 xmlData.write("\"" + str(key) + "\">" + str(value) + '</string>' + "\n") 
+
+                prevKey = key
+
+                for i in range(len(keys)):
+                    if keys[i] == prevKey:
+                        xmlData.write("ERROR")
+                        raise SystemExit("Duplicated key value: " + str(keys[i]) + "; aborting app.")
+
+                keys.append(key)
+
+            keys.clear()
 
             xmlData.write('</resources>' + "\n")
             xmlData.close()
 
 main()
-
-
-
-
-
-
-
-
-# CSV STUFF
-# import csv
-# csvFiles = [files for files in os.listdir('.') if files.endswith('.csv') or files.endswith('.CSV')]
-
-# def csv_to_xml():
-#     for csvFile in csvFiles:
-#         xmlFile = csvFile[:-4] + '.xml'
-#         csvData = csv.reader(open(csvFile))
-#         xmlData = open(xmlFile, 'w')
-#         # there must be only one top-level tag
-#         xmlData.write('<resources>' + "\n")
-#         rowNum = 0
-
-#         # First row of the csv files must be the header
-#         for row in csvData:
-#             if rowNum == 0:
-#                 tags = row
-#                 # replace spaces w/ underscores in tag names
-#                 for i in range(len(tags)):
-#                     tags[i] = tags[i].replace(' ', '_')
-#             else:
-#                 xmlData.write('    <string name=',)
-#                 # for i in range(len(tags)):
-#                 for i in range(1):
-#                     xmlData.write("\"" + tags[i] + '_' + row[i] + "\">" + row[1] + '</string>' + "\n")
-#             rowNum += 1
-            
-#         xmlData.write('</resources>' + "\n")
-#         xmlData.close()
